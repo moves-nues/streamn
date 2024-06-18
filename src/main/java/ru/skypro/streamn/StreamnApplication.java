62d85cee35d4356c8bcbbf73b3e4e8286c7676a9
moves <movesnues@gmail.com>
2024-06-18 09:53:51 +0300

    Doing homework

---
+++ src/main/java/ru/skypro/streamn/StreamnApplication.java
@@ -0,0 +1,13 @@
+package ru.skypro.streamn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/skypro/streamn/StreamnApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.streamn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
